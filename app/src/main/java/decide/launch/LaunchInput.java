7a6f8014c1a69959af994c6f5542b11e99efbd6d
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-28 20:14:38 +0100

    Add LaunchInput class
    An immutable class containing variables passed to the input of decide
    function. (+ getter functions for each variable)

---
+++ app/src/main/java/decide/launch/LaunchInput.java
@@ -0,0 +1,40 @@
+package decide.launch;
+
+import decide.utils.Point;
+
+// immutable class containing the input variables for "decide" function
+public final class LaunchInput {
+    private final int numPoints;
+    private final Point[] points;
+    private final Parameters params;
+    private final Connectors[][] lcm;
+    private final boolean[] puv;
+
+    public LaunchInput(int numPoints,
+                        Point[] points,
+                        Parameters params,
+                        Connectors[][] lcm,
+                        boolean[] puv) {
+        this.numPoints = numPoints;
+        this.points = points;
+        this.params = params;
+        this.lcm = lcm;
+        this.puv = puv;
+    }
+
+    public Point[] getPoints() {
+        return points;
+    }
+
+    public Parameters getParams() {
+        return params;
+    }
+
+    public Connectors[][] getLCM() {
+        return lcm;
+    }
+
+    public boolean[] getPUV() {
+        return puv;
+    }
+}
