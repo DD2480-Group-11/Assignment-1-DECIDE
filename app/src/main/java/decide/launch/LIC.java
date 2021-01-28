cb22c9e8cab810942ea81cf9a45d9e77d0cbdefd
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-28 20:13:25 +0100

    Add LIC class
    Contains a boolean function to evaluate each condition of the LICs.

---
+++ app/src/main/java/decide/launch/LIC.java
@@ -0,0 +1,80 @@
+package decide.launch;
+
+public final class LIC {
+
+    private static boolean isCondition0() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition1() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition2() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition3() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition4() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition5() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition6() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+
+    private static boolean isCondition7() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition8() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition9() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition10() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition11() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition12() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition13() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+
+    private static boolean isCondition14() {
+        // TODO: add appropriate method parameters
+        return false;
+    }
+}
