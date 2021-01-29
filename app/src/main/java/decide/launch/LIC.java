3fdcf19d5086fed9accfed1682a0cd79ce047c2d
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-29 21:56:12 +0100

    Add method implementation of LIC 0

--- app/src/main/java/decide/launch/LIC.java
+++ app/src/main/java/decide/launch/LIC.java
@@ -5,10 +5,22 @@ import decide.utils.Point;
 // conditions are used to calculate the CMV
 public final class LIC {
 
-    public static boolean isCondition0() {
-        // TODO: add appropriate method parameters
+    public static boolean isCondition0(Point[] points, double length) {
+
+        if(length <= 0.0) 
+            return false;
+
+        for(int i = 0; i < points.length-2; i++) {
+            final Point first = points[i];
+            final Point second = points[i+1];
+            final double distBetween = Point.distBetween(first, second);
+
+            if(distBetween > length)
+                return true;
+        }
+
         return false;
-    }
+    } 
 
     public static boolean isCondition1() {
         // TODO: add appropriate method parameters
