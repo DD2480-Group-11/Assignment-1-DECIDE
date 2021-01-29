ed51c14666682e597b328b4f11c665915593ebe8
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-29 19:37:55 +0100

    Add method to Point.java that calculates distance between two points
    This is a standard method that returns the pythagorean distance between
    two points.

--- app/src/main/java/decide/utils/Point.java
+++ app/src/main/java/decide/utils/Point.java
@@ -11,4 +11,10 @@ public final class Point {
         this.x = x;
         this.y = y;
     }
+
+    public static double distBetween(Point a, Point b) {
+        final double xDiff = a.x - b.x;
+        final double yDiff = a.y - b.y;
+        return Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+    }
 }
