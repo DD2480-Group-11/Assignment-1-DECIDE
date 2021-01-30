e4d0c91a51481a143e27c2297d97c51e763ee628
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-30 16:51:32 +0100

    Add various methods
    Methods that operate on points/vectors. These are required to compute
    some of the LICs.
    
    Methods to:
    * calculate the angle between vectors
    * calculate dot product of two vectors
    * magnitude of vector
    * check for equality of vector (Note: remember this can not simply be done by
    double == double because of precision. Java has a built-in method
    Double.compare for this)

--- app/src/main/java/decide/utils/Point.java
+++ app/src/main/java/decide/utils/Point.java
@@ -12,9 +12,58 @@ public final class Point {
         this.y = y;
     }
 
+    /**
+     * @param a one of two points
+     * @param b one of two points
+     * @return the distance between point a and b
+     */
     public static double distBetween(Point a, Point b) {
         final double xDiff = a.x - b.x;
         final double yDiff = a.y - b.y;
         return Math.sqrt((xDiff * xDiff) + (yDiff * yDiff));
+    }
+
+    /**
+     * @param a one out of three points
+     * @param vertex The points which forms an angle with point a and b
+     * @param b one out of three points
+     * @return the angle formed at the vertex by the three points
+     */
+    public static double calculateAngle(Point a, Point vertex, Point b) {
+        Point u = new Point(a.x - vertex.x, a.y - vertex.y);
+        Point v = new Point(b.x - vertex.x, b.y - vertex.y);
+
+        double numerator = dotProduct(u, v);
+        double denominator = magnitude(u) * magnitude(v);
+
+        return Math.toDegrees(Math.acos((numerator / denominator)));
+    }
+
+    /**
+     * @param a a two dimensional vector
+     * @param b a two dimensional vector
+     * @return the dot product of the two vectors
+     */
+    public static double dotProduct(Point a, Point b) {
+        return (a.x * b.x) + (a.y * b.y);
+    }
+
+    /**
+     * @param p a two dimensional vector
+     * @return the magnitude of the given vector
+     */
+    public static double magnitude(Point p) {
+        return Math.sqrt((p.x * p.x) + (p.y * p.y));
+    }
+
+    /** 
+     * @param a one of two compared two dimensional points
+     * @param b one of two compared two dimensional points
+     * @return true if the points have the same x and y value
+     */
+    public static boolean isEqual(Point a, Point b) {
+        if((Double.compare(a.x, b.x) == 0) && (Double.compare(a.y, b.y) == 0))
+            return true;
+        return false;
     }
 }
