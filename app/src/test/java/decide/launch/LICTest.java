2741dc1dfa8083a4eba5ced7367f34ffb9d1520c
Daniel Halvarsson <dh@pop-os.localdomain>
2021-01-31 22:44:09 +0100

    Add tests for LIC 9

--- app/src/test/java/decide/launch/LICTest.java
+++ app/src/test/java/decide/launch/LICTest.java
@@ -179,5 +179,117 @@ class LICTest {
         assertTrue(LIC.isCondition4(3, 1, points));
     }
 
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 fails if outside range (0 <= epsilon < pi)")
+    //tests that isCondition9 fails if epsilon value outside of range
+    void testCondition9Range() {
+        Point[] points = {new Point(0.0, 0.0)};
+        double negEpsilon = -1.0;
+        double outOfMaxRangeEpsilon = 3.5;
 
+        assertAll(  "Assert within range (0 <= epsilon < pi)",
+                    () -> assertFalse(LIC.isCondition9(points, negEpsilon, 1, 1)),
+                    () -> assertFalse(LIC.isCondition9(points, outOfMaxRangeEpsilon, 1, 1)));
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 returns false if either C_PTS and D_PTS are (< 1).")
+    void testCondition9PassedPointsRequirements() {
+        Point[] points = {  new Point(0.0, 0.0),
+                            new Point(0.0, 0.0),
+                            new Point(0.0, 0.0),
+                            new Point(0.0, 0.0),
+                            new Point(0.0, 0.0)};
+        double epsilon = 1.0;
+        int p1 = 0;
+        int p2 = 1;
+
+        assertAll(  () -> assertFalse(LIC.isCondition9(points, epsilon, p1, p2)),
+                    () -> assertFalse(LIC.isCondition9(points, epsilon, p2, p1)));
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 fails when first point coincides with vertex.")
+    void testCondition9AngleUndefined1() {
+        Point[] points = {  new Point(1.0, 1.0),    // coincides with vertex
+                            new Point(0.0, 0.0),
+                            new Point(1.0, 1.0),
+                            new Point(0.0, 0.0),
+                            new Point(0.0, 0.0)};
+        double epsilon = 0.0;
+        int cPts = 1;
+        int dPts = 1;
+
+        boolean result = LIC.isCondition9(points, epsilon, cPts, dPts);
+
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 fails when second point coincides with vertex.")
+    void testCondition9AngleUndefined2() {
+        Point[] points = {  new Point(0.0, 0.0),   
+                            new Point(0.0, 0.0),
+                            new Point(1.0, 1.0),
+                            new Point(0.0, 0.0),
+                            new Point(1.0, 1.0)};   // coincides with vertex
+        double epsilon = 0.0;
+        int cPts = 1;
+        int dPts = 1;
+
+        boolean result = LIC.isCondition9(points, epsilon, cPts, dPts);
+
+        assertFalse(result);
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 is true when condition (angle < (pi - epsilon)) is satisfied")
+    void testCondition9ConditionSatisfied1() {
+        Point[] points = {  new Point(3.0, 1.0),
+                            new Point(15.0, 15.0),
+                            new Point(0.0, 0.0),
+                            new Point(15.0, 15.0),
+                            new Point(3.0, 1.15)};    // angle approx. 2.56 degrees
+        double epsilon = 0.1;
+        int cPts = 1;
+        int dPts = 1;
+
+        boolean result = LIC.isCondition9(points, epsilon, cPts, dPts);
+
+        assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 is true when condition (angle > (pi + epsilon)) is satisfied")
+    void testCondition9ConditionSatisfied2() {
+        Point[] points = {  new Point(2.0, 2.0),
+                            new Point(15.0, 15.0),
+                            new Point(15.0, 3.0),
+                            new Point(15.0, 15.0),
+                            new Point(5.0, 8.0)};    // angle approx. 59 degrees
+        double epsilon = 2.5;
+        int cPts = 1;
+        int dPts = 1;
+
+        boolean result = LIC.isCondition9(points, epsilon, cPts, dPts);
+
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("LIC::isCondition9::LIC 9 is false when condition is not satisfied.")
+    void testCondition9ConditionNotSatisfied() {
+        Point[] points = {  new Point(3.0, 1.0),
+                            new Point(15.0, 15.0),
+                            new Point(0.0, 0.0),
+                            new Point(15.0, 15.0),
+                            new Point(3.0, 1.15)};      // angle approx. 2.56 degrees
+        double epsilon = 2.5;                           // makes angle > (pi - epsilon)
+        int cPts = 1;
+        int dPts = 1;
+
+        boolean result = LIC.isCondition9(points, epsilon, cPts, dPts);
+
+        assertFalse(result);
+    }
 }
